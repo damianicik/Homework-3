2114fded7e3a5296bfd9caab211e4b1b0e1a7342
damianicik <volknatalia1986@yandex.ru>
2023-04-17 22:07:19 +0300

    DZ_3

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+/*double Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+return Math.Round(res, 2);
+}
+Console.Write("Введите координату по Х для первой точки ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по У для первой точки ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для первой точки ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Х для второй точки ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Y для второй точки ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для первой точки ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double distance = Distance3D(x1, y1, x2, y2, z1,z2);
+System.Console.WriteLine($"Расстояние между точками = {distance}");*/
+
+/*//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= number; i++) // for - необходим для создания дополнительной перменной(старт, стоп, шаг)ы
+{
+Console.Write($"{i * i * i} ");
+}*/
+
+
+/*//Задача 19 - Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int palindrom = number.Length;
+if (palindrom == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " - палиндром");
+    }
+    else
+    {
+        Console.WriteLine(number + " - не палиндром");
+    }
+}
+else/*/
+
+/*Задача 21 - Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+{
+    Console.WriteLine ("Число не пятизначное");
+}*/
